4de3929846c34ba39b890a1f719fe79946f7dae5
Jeppe Beier <jeppevinkel@gmail.com>
2026-05-03 22:24:49 +0200

    chore: update DSharpPlus to nightly version and comment out GuildVoiceState class

--- JoBot.Discord/Services/GuildVoiceState.cs
+++ JoBot.Discord/Services/GuildVoiceState.cs
@@ -1,12 +1,12 @@
-﻿using DSharpPlus.Voice;
-
-namespace JoBot.Discord.Services;
-
-public class GuildVoiceState
-{
-    public SemaphoreSlim Lock { get; } = new(1, 1);
-    public VoiceConnection? Connection { get; set; }
-    public ulong? ChannelId { get; set; }
-    public CancellationTokenSource? PlaybackCts { get; set; }
-    public bool IsPlaying => PlaybackCts is not null;
-}
+﻿// using DSharpPlus.Voice;
+//
+// namespace JoBot.Discord.Services;
+//
+// public class GuildVoiceState
+// {
+//     public SemaphoreSlim Lock { get; } = new(1, 1);
+//     public VoiceConnection? Connection { get; set; }
+//     public ulong? ChannelId { get; set; }
+//     public CancellationTokenSource? PlaybackCts { get; set; }
+//     public bool IsPlaying => PlaybackCts is not null;
+// }
