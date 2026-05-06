7ba6bb0754f594b1ad52272d2718d37cece49262
Jeppe Beier <jeppevinkel@gmail.com>
2026-05-06 22:04:06 +0200

    feat: add AllowedUsers attribute and check for user-specific command access
    - Introduced `AllowedUsersAttribute` for defining user group restrictions on commands.
    - Implemented `AllowedUsersCheck` to validate command access using configuration-defined user groups.

---
+++ JoBot.Discord/Attributes/AllowedUsersAttribute.cs
@@ -0,0 +1,10 @@
+﻿using DSharpPlus.Commands.ContextChecks;
+
+namespace JoBot.Discord.Attributes;
+
+public class AllowedUsersAttribute : ContextCheckAttribute
+{
+    public string GroupName { get; init; }
+
+    public AllowedUsersAttribute(string groupName) => GroupName = groupName;
+}
---
+++ JoBot.Discord/Checks/AllowedUsersCheck.cs
@@ -0,0 +1,27 @@
+﻿using DSharpPlus.Commands;
+using DSharpPlus.Commands.ContextChecks;
+using JoBot.Discord.Attributes;
+using Microsoft.Extensions.Configuration;
+
+namespace JoBot.Discord.Checks;
+
+public class AllowedUsersCheck : IContextCheck<AllowedUsersAttribute>
+{
+    private readonly IConfiguration _configuration;
+
+    public AllowedUsersCheck(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public ValueTask<string?> ExecuteCheckAsync(AllowedUsersAttribute attribute, CommandContext context)
+    {
+        var allowedUsers = _configuration
+            .GetSection($"Discord:AllowedUsers:{attribute.GroupName}")
+            .Get<ulong[]>() ?? [];
+
+        return allowedUsers.Contains(context.User.Id)
+            ? ValueTask.FromResult<string?>(null)
+            : ValueTask.FromResult<string?>($"You are not allowed to use this command.");
+    }
+}
